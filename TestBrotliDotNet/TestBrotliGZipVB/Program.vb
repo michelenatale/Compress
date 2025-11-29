e761d71031ccd03c028d17027ae825c7f439fdbf
michelenatale <104991886+michelenatale@users.noreply.github.com>
2025-11-29 15:50:17 +0100

    Update

---
+++ TestBrotliDotNet/TestBrotliGZipVB/Program.vb
@@ -0,0 +1,32 @@
+
+
+
+Imports TestBrotliGZipVB.michele.natale.Tests
+
+Public Module Program
+  Public Sub Main()
+    StartAsyncHelper.Start().
+      GetAwaiter().GetResult()
+
+    Console.WriteLine()
+    Console.WriteLine("Finish")
+    Console.ReadLine()
+  End Sub
+End Module
+
+Namespace michele.natale.Tests
+  Public Class StartAsyncHelper
+    Public Shared Async Function Start() As Task
+      Await TestCompressedNet()
+      Await TestFileCompressPackage()
+    End Function
+
+    Private Shared Async Function TestCompressedNet() As Task
+      Await CompressedNetTest.Start()
+    End Function
+
+    Private Shared Async Function TestFileCompressPackage() As Task
+      Await FileCompressPackageTest.Start()
+    End Function
+  End Class
+End Namespace
